6b62e77e5231eacdebc527f2994835c0eb430133
SimonSteinberger <simon.steinberger@outlook.com>
2014-12-17 18:36:39 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 jQuery-autoGrowInput
 ====================
 
-A lightweight jQuery plugin for making text input fields grow and shrink dynamically while typing
+A lightweight jQuery plugin for making text input fields grow and shrink dynamically while typing.
+
+Compatible with jQuery 1.7.0+ in Firefox, Safari, Chrome, Opera, Internet Explorer 7+. No dependencies except the jQuery library.
+Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+This plugin was developed by and for [Pixabay.com](http://pixabay.com/) - an international repository for sharing free public domain images.
+It is based on a Stackoverflow answer by James Padolsey (stackoverflow.com/questions/931207).
+We have implemented this plugin in production and we share this piece of software - in the spirit of Pixabay - freely with others.
+
+## Demo and Documentation
+
+http://goodies.pixabay.com/jquery/auto-grow-input/demo.html
+
+## Features
+
+* Lightweight: 1.0 kB of JavaScript - less than 0.6 kB gzipped
+* Autosize text input fields dynamically while typing
+* Set maxWidth and minWidth on the input fields
+
+## Changelog
+
+### Version 1.0.0 - 2014/12/17
+
+* Initial release
---
+++ jquery.auto-grow-input.js
@@ -0,0 +1,44 @@
+/*
+	jQuery autoGrowInput v1.0.0
+    Copyright (c) 2014 Simon Steinberger / Pixabay
+    Based on stackoverflow.com/questions/931207 (James Padolsey)
+    GitHub: https://github.com/Pixabay/jQuery-autoGrowInput
+	License: http://www.opensource.org/licenses/mit-license.php
+*/
+
+(function($){
+    $.fn.autoGrowInput = function(options){
+        var o = $.extend({ maxWidth: 500, minWidth: 20, comfortZone: 0 }, options),
+            event = 'oninput' in document.createElement('input') ? 'input' : 'keydown';
+        this.filter('input:text').each(function(){
+            var minWidth = o.minWidth || $(this).width(),
+                val = ' ',
+                input = $(this),
+                comfortZone = o.comfortZone ? o.comfortZone : parseInt($(this).css('fontSize')),
+                span = $('<span/>').css({
+                    position: 'absolute',
+                    top: -9999,
+                    left: -9999,
+                    width: 'auto',
+                    fontSize: input.css('fontSize'),
+                    fontFamily: input.css('fontFamily'),
+                    fontWeight: input.css('fontWeight'),
+                    letterSpacing: input.css('letterSpacing'),
+                    whiteSpace: 'nowrap'
+                }),
+                check = function(e){
+                    if (val === (val = input.val()) && !e.type == 'autogrow') return;
+                    span.html(val.replace(/&/g, '&amp;').replace(/\s/g, '&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+                    var newWidth = span.width() + comfortZone, mw = typeof(o.maxWidth) == "function" ? o.maxWidth() : o.maxWidth;
+                    if (newWidth > mw) newWidth = mw;
+                    else if (newWidth < o.minWidth) newWidth = o.minWidth;
+                    if (newWidth != input.width()) input.width(newWidth);
+                };
+            span.insertAfter(input);
+            $(this).on(event+'.autogrow autogrow', check);
+            // init on page load
+            check();
+        });
+        return this;
+    }
+}(jQuery));

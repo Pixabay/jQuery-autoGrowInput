8591d12164972e36978fb8a1ad43d2bdf666efd7
Thibault Leruitte <thibault@leruitte.name>
2015-04-30 12:16:35 +0200

    Support placeholder
    Set the width to the placeholder width if the input has no value.

--- jquery.auto-grow-input.js
+++ jquery.auto-grow-input.js
@@ -28,6 +28,7 @@
                 }),
                 check = function(e){
                     if (val === (val = input.val()) && !e.type == 'autogrow') return;
+                    if (!val) val = input.prop('placeholder');
                     span.html(val.replace(/&/g, '&amp;').replace(/\s/g, '&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
                     var newWidth = span.width() + comfortZone, mw = typeof(o.maxWidth) == "function" ? o.maxWidth() : o.maxWidth;
                     if (newWidth > mw) newWidth = mw;
